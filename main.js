6c8fc242812db48c1f708f31edb29baf89c2c4f4
Tamás Martinec <tamas.martinec@symbio.com>
2024-10-17 12:17:08 +0300

    refactor: rename 'words' to 'scrollText' and adjust text height and speed

--- main.js
+++ main.js
@@ -36,10 +36,9 @@ function updateCameraPosition() {
 }
 cameraDistanceSlider.addEventListener('input', updateCameraPosition);
 
-// Load words from file
-const words = `Artificial intelligence (AI), in its broadest sense, is intelligence exhibited by machines, particularly computer systems. It is a field of research in computer science that develops and studies methods and software that enable machines to perceive their environment and use learning and intelligence to take actions that maximize their chances of achieving defined goals.[1] Such machines may be called AIs.`;
+const scrollText = `Artificial intelligence (AI), in its broadest sense, is intelligence exhibited by machines, particularly computer systems. It is a field of research in computer science that develops and studies methods and software that enable machines to perceive their environment and use learning and intelligence to take actions that maximize their chances of achieving defined goals.[1] Such machines may be called AIs.`;
 
-const wordsArray = words.split(' ');
+const wordsArray = scrollText.split(' ');
 
 // Create text meshes
 const loader = new THREE.FontLoader();
@@ -53,7 +52,7 @@ loader.load('helvetiker_regular.typeface.json', function(font) {
     const textGeometry = new THREE.TextGeometry(word, {
       font: font,
       size: 1.5,
-      height: 0.2
+      height: 0.4
     });
 
     textGeometry.computeVertexNormals();    
@@ -77,7 +76,7 @@ loader.load('helvetiker_regular.typeface.json', function(font) {
   console.log('Starting Positions:', startingPositions);
 
   // Animation loop
-  const speed = 0.05;
+  const speed = 0.1;
   let allWordsExited = true;
   function animate() {
     requestAnimationFrame(animate);
